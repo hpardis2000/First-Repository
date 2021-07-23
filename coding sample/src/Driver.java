d372bedc5c647c351d618e4197a45c7e5f5430fe
Hamid Ghaderi <hghader@mail.ccsf.edu>
2021-07-22 18:27:12 -0700

    first commit uploading all file

---
+++ coding sample/src/Driver.java
@@ -0,0 +1,14 @@
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+public class Driver {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		System.out.println(NumberFormat.getPercentInstance().format(1235152.25));
+		
+		
+	}
+}
+
